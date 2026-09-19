29d87768fcbfa8ea12177f7015e14fa9fa4ee4d9
Rubén Santos <ribal@dontgiveahack.com>
2026-09-19 11:18:31 +0200

    nvim/pack: add opencode-nvim plugin

--- nvim/lua/pack.lua
+++ nvim/lua/pack.lua
@@ -7,7 +7,11 @@ vim.pack.add({
 	"https://github.com/nvim-mini/mini.pairs",
 	"https://github.com/nvim-treesitter/nvim-treesitter",
 	"https://github.com/windwp/nvim-ts-autotag",
-	"https://github.com/MeanderingProgrammer/render-markdown.nvim"
+	"https://github.com/MeanderingProgrammer/render-markdown.nvim",
+	{
+		src = "https://github.com/nickjvandyke/opencode.nvim",
+		version = vim.version.range("*"), -- latest stable release
+	}
 })
 
 -- dapui
@@ -32,3 +36,11 @@ require("nvim-ts-autotag").setup()
 
 -- render-markdown
 require("render-markdown").setup()
+
+-- opencode
+---@type opencode.Opts
+vim.g.opencode_opts = {
+	-- custom config
+}
+vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ") end, { desc = "Ask OpenCode..." })
+vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,       { desc = "Select OpenCode..." })
