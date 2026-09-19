1d92a4cd2f4b8045ad188a58350ea16b23df3e05
Rubén Santos <ribal@dontgiveahack.com>
2026-09-19 13:16:27 +0200

    nvim/diagnostics: support quickfix list

--- nvim/lua/diagnostics.lua
+++ nvim/lua/diagnostics.lua
@@ -10,4 +10,11 @@ vim.diagnostic.config({
 	}
 })
 
+-- Open floating line diagnostics
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+
+-- Open Quickfix List
+vim.keymap.set("n", "<leader>dl", function()
+	vim.diagnostic.setqflist()
+	vim.cmd("copen")
+end, { silent = true })
